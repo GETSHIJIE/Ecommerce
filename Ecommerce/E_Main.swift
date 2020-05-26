d5d16a488100146b69823afe7d61e9c8974173ee
黃仕杰 <>
2020-05-26 20:12:57 +0800

    update

--- Ecommerce/E_Main.swift
+++ Ecommerce/E_Main.swift
@@ -19,7 +19,7 @@ public class NSGetValue {
     }
     
     struct IP {
-        static let ip: String = "http://172.20.10.3:8080/";
+        static let ip: String = "http://127.0.0.1:8080/";
     }
     
     struct Php_Files {
